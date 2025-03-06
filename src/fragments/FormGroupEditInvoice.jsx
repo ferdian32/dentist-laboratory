83060054bb76a59e823e88409ec0e93c9249c733
ferdian32 <fierliferdiansyah0@gmail.com>
2025-03-06 20:38:32 +0700

    mengubah file FormGroupEditInvoice.jsx

--- src/fragments/FormGroupEditInvoice.jsx
+++ src/fragments/FormGroupEditInvoice.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { __global__ } from "../__config__";
 import Button from "../elements/button";
+import Swal from "sweetalert2";
 import { __httpClient__ } from "../lib/http";
 const FormGroupEditInvoice = ({ x, prms }) => {
   const { formDataInvoice, setFormDataInvoice } = useContext(__global__);
@@ -12,11 +13,13 @@ const FormGroupEditInvoice = ({ x, prms }) => {
   };
   const _2x3al = async () => {
     try {
+      const { no_invoice, nama_customer, alamat, pasien, order_id } = formDataInvoice;
       const data = {
-        no_invoice: formDataInvoice.no_invoice,
-        nama_customer: formDataInvoice.nama_customer,
-        alamat: formDataInvoice.alamat,
-        pasien: formDataInvoice.pasien
+        no_invoice,
+        nama_customer,
+        alamat,
+        pasien,
+        order_id
       }
       const response = await __httpClient__.patch(`${import.meta.env.VITE_BASE_URL_NVC}/${prms}`, data);
       if (response.status === 200) {
@@ -30,7 +33,7 @@ const FormGroupEditInvoice = ({ x, prms }) => {
           }
         });
       } else {
-        alert('Kontol');
+        alert('Failed');
       }
       return response;
     } catch (error) {
@@ -40,45 +43,69 @@ const FormGroupEditInvoice = ({ x, prms }) => {
   return (
     <div >
       <section>
-        <input
-          onChange={changeEvent}
-          className="border border-black w-full rounded-md py-1 px-3 mt-3"
-          type="text"
-          name="no_invoice"
-          id="no_invoice"
-          placeholder="Masukan No Invoice"
-          value={formDataInvoice.no_invoice || ""}
-        />
-        <input
-          onChange={changeEvent}
-          className="border border-black w-full rounded-md py-1 px-3 mt-3"
-          type="text"
-          name="nama_customer"
-          id="nama_customer"
-          placeholder="Masukan Nama Customer"
-          value={formDataInvoice.nama_customer || ""}
-        />
-        <input
-          onChange={changeEvent}
-          className="border border-black w-full rounded-md py-1 px-3 mt-3"
-          type="text"
-          name="alamat"
-          id="alamat"
-          placeholder="Alamat .."
-          value={formDataInvoice.alamat || ""}
-        />
-        <input
-          onChange={changeEvent}
-          className="border border-black w-full rounded-md py-1 px-3 mt-3"
-          type="text"
-          name="pasien"
-          id="pasien"
-          placeholder="Pasien"
-          value={formDataInvoice.pasien || ""}
-        />
+        <div>
+          <label htmlFor="no_invoice">No Invoice</label>
+          <input
+            onChange={changeEvent}
+            className="border border-black w-full rounded-md py-1 px-3 mt-3"
+            type="text"
+            name="no_invoice"
+            id="no_invoice"
+            placeholder="Masukan No Invoice"
+            value={formDataInvoice.no_invoice || ""}
+          />
+        </div>
+        <div>
+          <label htmlFor="nama_customer">Nama Customer</label>
+          <input
+            onChange={changeEvent}
+            className="border border-black w-full rounded-md py-1 px-3 mt-3"
+            type="text"
+            name="nama_customer"
+            id="nama_customer"
+            placeholder="Masukan Nama Customer"
+            value={formDataInvoice.nama_customer || ""}
+          />
+        </div>
+        <div>
+          <label htmlFor="alamat">Alamat</label>
+          <input
+            onChange={changeEvent}
+            className="border border-black w-full rounded-md py-1 px-3 mt-3"
+            type="text"
+            name="alamat"
+            id="alamat"
+            placeholder="Alamat .."
+            value={formDataInvoice.alamat || ""}
+          />
+        </div>
+        <div>
+          <label htmlFor="pasien">Pasien</label>
+          <input
+            onChange={changeEvent}
+            className="border border-black w-full rounded-md py-1 px-3 mt-3"
+            type="text"
+            name="pasien"
+            id="pasien"
+            placeholder="Pasien"
+            value={formDataInvoice.pasien || ""}
+          />
+        </div>
+        <div>
+          <label htmlFor="order_id">order Id</label>
+          <input
+            onChange={changeEvent}
+            className="border border-black w-full rounded-md py-1 px-3 mt-3"
+            type="text"
+            name="order_id"
+            id="order_id"
+            placeholder="Order Id"
+            value={formDataInvoice.order_id || ""}
+          />
+        </div>
       </section>
       <div>
-        <Button title="Simpan Update Data" onClick={_2x3al} type="submit" className="bg-rose-500 text-slate-50 py-2 ml-3 px-3 rounded-md cursor-pointer"></Button>
+        <Button title="Simpan Update Data" onClick={_2x3al} type="submit" className="bg-rose-500 text-slate-50 py-2 mt-3 px-3 rounded-md cursor-pointer"></Button>
       </div>
     </div>
   )
