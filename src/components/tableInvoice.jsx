bd2d892dbe639376788a87d9ef842ad30fc30dba
ferdian32 <fierliferdiansyah0@gmail.com>
2025-03-06 20:12:59 +0700

    mengubah file tableInvoice.jsx

--- src/components/tableInvoice.jsx
+++ src/components/tableInvoice.jsx
@@ -12,7 +12,7 @@ const TableInvoice = ({ column, nvc }) => {
     };
     get();
   }, []);
-  const handleDeleteButton = async (no) => {
+  const _x20xa2 = async (no) => {
     try {
       const result = await Swal.fire({
         title: "Are you Sure?",
@@ -126,7 +126,7 @@ const TableInvoice = ({ column, nvc }) => {
                 <td >
 
                   <div className="flex gap-x-3  p-3">
-                    <button className="bg-rose-500 cursor-pointer text-slate-50 py-1 px-3 rounded-md" onClick={() => handleDeleteButton(data.no_invoice)}>
+                    <button className="bg-rose-500 cursor-pointer text-slate-50 py-1 px-3 rounded-md" onClick={() => _x20xa2(data.no_invoice)}>
                       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 7h16m-10 4v6m4-6v6M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2l1-12M9 7V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v3" /></svg>
                     </button>
                     <Link to={`/editDataInvoice/${data.id_invoice}`} className="bg-orange-500 cursor-pointer text-slate-50 py-1 px-3 rounded-md">
